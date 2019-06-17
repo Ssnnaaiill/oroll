069d5fdaffb653cbbefdc4b41bbc5703e2f77138
Ssnnaaiill <newdk3025@gmail.com>
2019-06-17 03:23:05 -0700

    회원가입 폼 구현

--- oroll/views.py
+++ oroll/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
-from django.contrib.auth.forms import UserCreationForm
+#from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import CreateUserForm
 # Create your views here.
 
 class IndexView(TemplateView):
@@ -10,8 +11,8 @@ class IndexView(TemplateView):
 
 class CreateUserView(CreateView):
     template_name = 'registration/signup.html'
-    # form_class = CreateUserForm
-    form_class = UserCreationForm
+    form_class = CreateUserForm
+    #form_class = UserCreationForm
     success_url = reverse_lazy('create_user_done')
 
 class RegisteredView(TemplateView):
